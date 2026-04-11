f2f8c5ad6c4087c593fc30d5e88d44a849a31e28
Rallevar <nkeenan6969@gmail.com>
2026-04-10 22:50:13 -0500

    Added products controller with index and show views. Added to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :products, only: [:index, :show]
+
   get "main/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
